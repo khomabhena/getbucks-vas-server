e38bb5d8e0a9e1dc6d448916b1f9563d8acdc894
khomabhena <colwanymab@gmail.com>
2026-02-05 20:24:21 +0200

    Enable CORS for tester

--- src/index.js
+++ src/index.js
@@ -27,6 +27,15 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.static('public'));
 app.use(morgan('combined'));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  return next();
+});
 
 const apiLimiter = rateLimit({
   windowMs: 60 * 1000,
